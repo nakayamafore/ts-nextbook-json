ea1713915712df46225eeb8a0b8ebdee3bfb676e
kouichi nakayama <mable.index.matrix@gmail.com>
2022-09-07 08:21:14 +0900

    フォーマット後

--- server.js
+++ server.js
@@ -1,67 +1,70 @@
 const process = require("process");
-const jsonServer = require('json-server');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const express = require('express');
+const jsonServer = require("json-server");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const express = require("express");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8000;
 const authUser = {
-  id: '1',
-  username: 'taketo',
-  displayName: 'Taketo Yoshida',
-  email: 'taketo@example.com',
-  profileImageUrl: '/users/1.png',
+  id: "1",
+  username: "taketo",
+  displayName: "Taketo Yoshida",
+  email: "taketo@example.com",
+  profileImageUrl: "/users/1.png",
   description:
-    'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
 };
 
 server.use(cookieParser());
 server.use(express.json());
 
-server.post('/auth/signin', (req, res) => {
+server.post("/auth/signin", (req, res) => {
   if (
-    !(req.body['username'] === 'user' && req.body['password'] === 'password')
+    !(
+      req.body["username"] === "mable.index.matrix@gmail.com" &&
+      req.body["password"] === "bagabondo13"
+    )
   ) {
     return res.status(401).json({
-      message: 'Username or password are incorrect',
+      message: "Username or password are incorrect",
     });
   }
 
-  res.cookie('token', 'dummy_token', {
+  res.cookie("token", "dummy_token", {
     maxAge: 3600 * 1000,
     httpOnly: true,
   });
   res.status(201).json(authUser);
 });
 
-server.post('/auth/signout', (req, res) => {
-  res.cookie('token', '', {
+server.post("/auth/signout", (req, res) => {
+  res.cookie("token", "", {
     maxAge: 0,
     httpOnly: true,
   });
   res.status(200).json({
-    message: 'Sign out successfully',
+    message: "Sign out successfully",
   });
 });
 
-server.post('/purchases', (req, res) => {
-  if (req.cookies['token'] !== 'dummy_token') {
+server.post("/purchases", (req, res) => {
+  if (req.cookies["token"] !== "dummy_token") {
     return res.status(401).json({
-      message: 'Unauthorized',
+      message: "Unauthorized",
     });
   }
 
   res.status(201).json({
-    message: 'ok',
+    message: "ok",
   });
 });
 
-server.get('/users/me', (req, res) => {
-  if (req.cookies['token'] !== 'dummy_token') {
+server.get("/users/me", (req, res) => {
+  if (req.cookies["token"] !== "dummy_token") {
     return res.status(401).json({
-      message: 'Unauthorized',
+      message: "Unauthorized",
     });
   }
 
@@ -77,5 +80,5 @@ server.listen(port, (err) => {
     return;
   }
   console.log("Start listening...");
-  console.log('http://localhost:' + port);
+  console.log("http://localhost:" + port);
 });
